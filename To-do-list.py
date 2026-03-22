62aacd16ead39e8869264b0d17141713d371eaca
GabrielB <gabrielbragaevan503@gmail.com>
2026-03-22 17:36:02 -0300

    To-do-list-with-checker-and-init-metod-without-db

--- To-do-list.py
+++ To-do-list.py
@@ -1,25 +1,32 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title('To-do-list')
-window.geometry("350x400")
-tk.Label(window, text="Check your to-do-list and complete the assigments!").pack()
+class ToDoList:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title('To-do-list')
+        self.window.geometry("350x400")
+        tk.Label(self.window, text="Check your to-do-list and complete the assigments!").pack()
+        tk.Button(self.window, text="Add an assigment", command=self.add_assignment).pack()
+        self.window.mainloop()
 
-def add_assigment():
-    def new_assigment():
-        task_frame = tk.Frame(window)
+    def add_assignment(self):
+        self.assignment_entry= tk.Entry(self.window)
+        self.assignment_entry.pack()
+        self.confirmbtn = tk.Button(self.window, text="Confirm", command=self.new_assignment)
+        self.confirmbtn.pack()
+
+    def new_assignment(self):
+        task_frame = tk.Frame(self.window)
         task_frame.pack()
-        assigment=assigment_entry.get()
-        assigment_entry.pack_forget() 
-        confirmbtn.pack_forget()   
-        tk.Label(task_frame, text=f"{assigment}").pack(side=tk.RIGHT)
+        assignment = self.assignment_entry.get()
+        self.assignment_entry.pack_forget() 
+        self.confirmbtn.pack_forget()   
+        tk.Label(task_frame, text=f"{assignment}").pack(side=tk.RIGHT)
         variavel_check = tk.BooleanVar()
         check=tk.Checkbutton(task_frame, variable=variavel_check).pack(side=tk.LEFT)
-    assigment_entry= tk.Entry(window)
-    assigment_entry.pack()
-    confirmbtn = tk.Button(window, text="Confirm", command=new_assigment)
-    confirmbtn.pack()
 
-tk.Button(window, text="Add an assigment", command=add_assigment).pack()
+if __name__ == "__main__":
+     ToDoList()
 
-window.mainloop()
+
+    
